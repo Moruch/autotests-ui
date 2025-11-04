5e0a8c9416b79a97fe8bc338587ce049ce2dc01c
Moruch <Moruchgithub@mail.ru>
2025-11-04 10:34:37 +0300

    Интеграция Pytest и Playwright

---
+++ tests/test_registration.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import sync_playwright, expect
+
+def test_successful_registratio():
+    with sync_playwright() as playwright:
+        # Открываем браузер и создаем новую страницу
+        browser = playwright.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        # Переходим на страницу входа
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+        # Заполняем поле email
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill("user.name@gmail.com")
+
+        # Заполняем поле Username
+        user_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        user_input.fill("user")
+
+        # Заполняем поле Password
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill("password")
+
+        # Нажимаем на кнопку Registration
+        login_button = page.get_by_test_id('registration-page-registration-button')
+        login_button.click()
+
+        # Проверяем наличие заголовка Dashboard
+        dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+        expect(dashboard_title).to_be_visible()
+        expect(dashboard_title).to_have_text("Dashboard")
